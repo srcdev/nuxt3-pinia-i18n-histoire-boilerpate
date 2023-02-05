65f5ea2c483cf3f8dc0423494c52955abcda4ef9
Simon Cornforth <simon.cornforth@srcdev.co.uk>
2023-02-05 21:48:12 +0000

    Updated root store

--- stores/store.root.ts
+++ stores/store.root.ts
@@ -6,22 +6,6 @@ export const useRootStore = defineStore({
   id: 'rootStore',
   state: (): IRootStore => ({
     validatorLocale: 'en-GB',
-    cssVars: {
-      upvc: {
-        panelBgColor: 'rgba(38, 91, 150, 0.73)',
-        panelBgImg:
-          'linear-gradient(120deg, rgba(231, 240, 249, 0.31), transparent)',
-      },
-      aluminium: {
-        panelBgColor: 'rgba(60, 60, 60, 0.9)',
-        panelBgImg:
-          'linear-gradient(145deg, rgba(249, 231, 231, 0.09), transparent)',
-      },
-    },
   }),
-  persist: {
-    key: 'rootStore',
-    storage: window.sessionStorage,
-  },
   actions: rootActions,
 })
--- types/types.rootStore.ts
+++ types/types.rootStore.ts
@@ -1,10 +1,3 @@
-export interface ICssVars {
-  [key: string]: {
-    panelBgColor: string
-    panelBgImg: string
-  }
-}
 export interface IRootStore {
   validatorLocale: string
-  cssVars: ICssVars
 }
